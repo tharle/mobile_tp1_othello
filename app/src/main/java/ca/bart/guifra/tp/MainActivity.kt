ae8b0b1e954b131f5381f4922c2ef3fceefaa54c
Tharle Josefi de Camargo <tharles.josefidecamargo@gmail.com>
2024-07-11 16:50:22 -0400

    Added cell valids, turn base,

--- app/src/main/java/ca/bart/guifra/tp/MainActivity.kt
+++ app/src/main/java/ca/bart/guifra/tp/MainActivity.kt
@@ -9,11 +9,16 @@ import ca.bart.guifra.tp.databinding.ActivityMainBinding
 
 data class Cell(var idPlayer: Int = -1)
 
-data class Player(var idPlayer: Int, var discId: Int = R.drawable.filled_disc, var score: Int = 0 ,var isIA: Boolean = false)
+data class Player(
+    var idPlayer: Int,
+    var idDisc: Int = R.drawable.filled_disc,
+    var score: Int = 0,
+    var isIA: Boolean = false
+)
 
 data class Model(
     val grid: Array<Cell> = Array(64) { Cell() },
-    val validIdCells: List<Int> = listOf<Int>(1),
+    val validIdCells: ArrayList<Int> = arrayListOf<Int>(),
     val players: Array<Player> = arrayOf(
         Player(0, R.drawable.filled_disc_player_0),
         Player(1, R.drawable.filled_disc_player_1),
@@ -29,8 +34,12 @@ class MainActivity : Activity() {
 
         const val TAG = "MainActivity"
 
-        const val NB_COLUMNS = 3
-        const val NB_ROWS = 3
+        const val NB_COLUMNS = 8
+        const val NB_ROWS = 8
+        const val ID_PLAYER_EMPTY = -1
+        const val ID_PLAYER_VALID = 666
+        val ID_DISC_EMPTY = R.drawable.empty_disc
+        val ID_DISC_VALID = R.drawable.filled_disc
 
         val NORTH = Pair(0, -1)
         val WEST = Pair(-1, 0)
@@ -53,54 +62,121 @@ class MainActivity : Activity() {
             }
         }
 
-        binding.player0Button.setOnClickListener({nextPlayer()})
-        binding.player1Button.setOnClickListener({nextPlayer()})
-        binding.player2Button.setOnClickListener({nextPlayer()})
-        binding.player3Button.setOnClickListener({nextPlayer()})
+        binding.player0Button.setOnClickListener({ nextPlayer() })
+        binding.player1Button.setOnClickListener({ nextPlayer() })
+        binding.player2Button.setOnClickListener({ nextPlayer() })
+        binding.player3Button.setOnClickListener({ nextPlayer() })
 
         initGrid()
     }
 
-    fun initGrid(){
+    fun initGrid() {
 
         model.grid.forEach {
-            it.idPlayer = -1
+            it.idPlayer = ID_PLAYER_EMPTY
         }
         model.grid[27].idPlayer = 0
         model.grid[28].idPlayer = 1
         model.grid[35].idPlayer = 3
         model.grid[36].idPlayer = 2
 
+        calculValidsCells()
         refresh()
     }
 
-    fun onButtonClicked(index:Int) {
-        val coordinates:Pair<Int, Int> = index.toCoordinates()
+    fun onButtonClicked(index: Int) {
+        val coordinates: Pair<Int, Int> = index.toCoordinates()
 
         Log.d(TAG, "onButtonClicked($index) = $coordinates")
         Log.d(TAG, "coordinates.x = ${coordinates.x}")
         Log.d(TAG, "coordinates.y = ${coordinates.y}")
         Log.d(TAG, "coordinates.toIndex() = ${coordinates.toIndex()}")
 
-        model.grid[index + (NORTH + EAST)]?.idPlayer = model.currentIdPlayer
-
-
-
         // is this a valid move?
-        if(!model.validIdCells.contains(index)) return;
+        if (!model.validIdCells.contains(index)) return;
+
 
         // if yes, DO THE MOVE
         model.grid[index].idPlayer = model.currentIdPlayer
+
+        // Get all from the direction
+        //model.grid[index + (NORTH + EAST)]?.idPlayer = model.currentIdPlayer
+
 
         // player++ <--- new valid move list
         nextPlayer();
     }
 
-    fun nextPlayer()
-    {
+    fun nextPlayer() {
         model.currentIdPlayer++
-        model.currentIdPlayer = if(model.currentIdPlayer < model.players.size) model.currentIdPlayer else 0
+        model.currentIdPlayer =
+            if (model.currentIdPlayer < model.players.size) model.currentIdPlayer else 0
+
+        calculValidsCells()
+
         refresh()
+    }
+
+    fun addValidCell(index: Int) {
+        model.validIdCells.add(index)
+        model.grid[index].idPlayer = ID_PLAYER_VALID
+    }
+
+    fun calculValidsCells() {
+        cleanCellsValids()
+
+        val idPlayer = model.currentIdPlayer
+
+        var index = 0
+        model.grid.forEach {
+            if (it.idPlayer == idPlayer) {
+
+                //val direction = SOUTH
+
+                // Check all valids
+                for(x in -1 .. 1) {
+                    for(y in -1 .. 1) {
+                        val direction = Pair(x, y)
+
+                        if(direction.x == 0 && direction.y == 0) continue
+
+                        val idsCellEnemiesFound: ArrayList<Int> = ArrayList()
+                        var coordinates: Pair<Int, Int> = index.toCoordinates().plus(direction)
+                        while (
+                            (direction.x != 0 && coordinates.x in 0 until NB_COLUMNS)
+                            || (direction.y != 0 && coordinates.y in 0 until NB_ROWS)) {
+                            val idCell = coordinates.toIndex()
+                            val playerLoop = model.grid[idCell]
+
+                            if(playerLoop.idPlayer == ID_PLAYER_EMPTY) {
+                                if(idsCellEnemiesFound.isNotEmpty()) addValidCell(idCell)
+                                break // if found empty space, end it
+                            }  else if(playerLoop.idPlayer == idPlayer) {
+                                idsCellEnemiesFound.clear()  // if same id, restart list of cells to convert
+                            }
+                            else {
+                                idsCellEnemiesFound.add(idCell) // add to list to convert all
+                            }
+
+                            coordinates = coordinates.plus(direction)
+                        }
+                    }
+                }
+
+                //Converts all we found in our way to empty space
+                //idCellsToConvert.forEach{idCell -> model.grid[idCell]?.idPlayer = idPlayer}
+
+            }
+
+            index++
+        }
+    }
+
+    fun cleanCellsValids() {
+        model.validIdCells.clear()
+        model.grid
+            .filter { cell -> cell.idPlayer == ID_PLAYER_VALID }
+            .forEach { cell -> cell.idPlayer = ID_PLAYER_EMPTY }
     }
 
     fun GetCurrentPlayer(): Player {
@@ -111,11 +187,10 @@ class MainActivity : Activity() {
 
         val currentPlayer = GetCurrentPlayer();
         // update display of current player
-        binding.turnIcon.setBackgroundResource(currentPlayer.discId)
+        binding.turnIcon.setBackgroundResource(currentPlayer.idDisc)
 
         // update player corners (score, state of skip button)
         refreshPlayers();
-        refreshValidCells();
 
         model.grid.asSequence().zip(binding.grid.children)
             .forEach { (cell, button) ->
@@ -123,46 +198,52 @@ class MainActivity : Activity() {
                 // show valid moves (from model?)
 
                 button.setBackgroundResource(
-                    // probablement un "when" pour vous..
-                    if (cell.idPlayer == -1) R.drawable.empty_disc
-                    else model.players[cell.idPlayer].discId
+                    when (cell.idPlayer) {
+                        ID_PLAYER_EMPTY -> ID_DISC_EMPTY
+                        ID_PLAYER_VALID -> ID_DISC_VALID
+                        else -> model.players[cell.idPlayer].idDisc
+                    }
                 )
             }
     }
 
     fun refreshPlayers() {
         var player: Player = model.players[0]
-        binding.player0Icon.setBackgroundResource(player.discId)
+        binding.player0Icon.setBackgroundResource(player.idDisc)
         binding.player0Score.text = player.score.toString()
         binding.player0Button.isEnabled = model.currentIdPlayer == player.idPlayer
 
         player = model.players[1]
-        binding.player1Icon.setBackgroundResource(player.discId)
+        binding.player1Icon.setBackgroundResource(player.idDisc)
         binding.player1Score.text = player.score.toString()
         binding.player1Button.isEnabled = model.currentIdPlayer == player.idPlayer
 
         player = model.players[2]
-        binding.player2Icon.setBackgroundResource(player.discId)
+        binding.player2Icon.setBackgroundResource(player.idDisc)
         binding.player2Score.text = player.score.toString()
         binding.player2Button.isEnabled = model.currentIdPlayer == player.idPlayer
 
         player = model.players[3]
-        binding.player3Icon.setBackgroundResource(player.discId)
+        binding.player3Icon.setBackgroundResource(player.idDisc)
         binding.player3Score.text = player.score.toString()
         binding.player3Button.isEnabled = model.currentIdPlayer == player.idPlayer
     }
 }
 
-private fun refreshValidCells(){
-    // TODO refreshir les cells qui sont valides pour le jouer du tour
-}
 
+private fun Int.toCoordinates() =
+    Pair(this % MainActivity.NB_COLUMNS, this / MainActivity.NB_COLUMNS)
 
-private fun Int.toCoordinates() = Pair(this % MainActivity.NB_COLUMNS, this / MainActivity.NB_COLUMNS)
 private val Pair<Int, Int>.x get() = first
 private val Pair<Int, Int>.y get() = second
-private fun Pair<Int, Int>.toIndex() = y * MainActivity.NB_COLUMNS + x
-private operator fun Pair<Int, Int>.plus(other:Pair<Int, Int>) = Pair(this.x + other.x, this.y + other.y)
+private fun Pair<Int, Int>.toIndex() : Int {
+    val campX = x.coerceIn(0, MainActivity.NB_COLUMNS - 1)
+    val campY = y.coerceIn(0, MainActivity.NB_ROWS - 1)
+    return campY * MainActivity.NB_COLUMNS + campX
+}
+private operator fun Pair<Int, Int>.plus(other: Pair<Int, Int>) =
+    Pair(this.x + other.x, this.y + other.y)
+
 private operator fun Int.plus(direction: Pair<Int, Int>): Int? =
     (toCoordinates() + direction).let { asCoordinates ->
 
